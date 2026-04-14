0be6ae16d0389ed47b00e681b05b574a37add9a5
Sulthon Abdillah <sulthonabdillah32@gmail.com>
2026-04-14 18:06:41 +0700

    fix user model role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,20 +30,21 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required','string','max:255'],
             'email' => ['required','string','email','max:255','unique:users,email'],
-            'password' => ['required','confirmed',Rules\Password::defaults()],
+            'password' => ['required','confirmed', Rules\Password::defaults()],
         ]);
 
-        // 🔥 SIMPAN KE MYSQL SAJA (TANPA FIREBASE)
+        // ✅ SIMPAN KE MYSQL (FULL TANPA FIREBASE)
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'user', // 🔥 PENTING untuk sistem admin nanti
         ]);
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect('/'); // atau route('home')
+        return redirect()->route('home'); // lebih rapi daripada '/'
     }
 }
