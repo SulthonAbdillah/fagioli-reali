c867f5c3da96566e1497c730d1ac96e1af186748
Sulthon Abdillah <sulthonabdillah32@gmail.com>
2026-04-16 00:04:59 +0700

    fix css production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
 <link rel="stylesheet"
 href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<!-- 🔥 FIX UTAMA: DIRECT PATH -->
+<link rel="stylesheet" href="/css/style.css?v={{ time() }}">
 
 </head>
 
@@ -38,93 +39,6 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.c
 </div>
 
 </footer>
-
-
-{{-- CART SIDEBAR --}}
-
-<div class="offcanvas offcanvas-end" tabindex="-1" id="cartSidebar">
-
-<div class="offcanvas-header">
-
-<h5 class="offcanvas-title">Your Cart</h5>
-
-<button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-
-</div>
-
-<div class="offcanvas-body">
-
-@php
-$cart = session('cart', []);
-$total = 0;
-@endphp
-
-
-@forelse($cart as $id => $item)
-
-<div class="d-flex mb-3 align-items-center">
-
-@if($item['image'])
-
-<img src="{{ $item['image'] }}" width="60">
-
-@endif
-
-<div class="ms-3">
-
-<strong>{{ $item['name'] }}</strong>
-
-<br>
-
-Qty: {{ $item['quantity'] }}
-
-<br>
-
-Rp {{ number_format($item['price']) }}
-
-</div>
-
-</div>
-
-@php
-$total += $item['price'] * $item['quantity'];
-@endphp
-
-<form action="{{ route('cart.remove',$id) }}" method="POST">
-
-@csrf
-
-<button class="btn btn-sm btn-danger mb-3">
-Remove
-</button>
-
-</form>
-
-@empty
-
-<p>Cart is empty</p>
-
-@endforelse
-
-
-<hr>
-
-<h5>Total: Rp {{ number_format($total) }}</h5>
-
-<form action="{{ route('checkout') }}" method="POST">
-
-@csrf
-
-<button class="btn btn-dark w-100 mt-3">
-Checkout
-</button>
-
-</form>
-
-</div>
-
-</div>
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
